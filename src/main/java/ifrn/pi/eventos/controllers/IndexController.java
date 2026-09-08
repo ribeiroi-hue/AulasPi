517e7a965e87ab28436ad43291a61e8ef7e7d0ad
MARIA ISABEL PINHEIRO RIBEIRO OLIVEIRA <20231204010011@ifrn.local>
2026-09-08 15:24:38 -0300

    Criando a listagem de Eventos

--- src/main/java/ifrn/pi/eventos/controllers/IndexController.java
+++ src/main/java/ifrn/pi/eventos/controllers/IndexController.java
@@ -5,9 +5,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
-	@RequestMapping("/")
-	public String index() {
-		
-		return "home";
-	}
+
+@RequestMapping("/")
+public String index() {
+System.out.println("Chamou o método index");
+return "home";
+return "redirect:/eventos";
+}
+
 }
